34784a84f6f148f665822fc092da8322a66853d3
Lars Wenk <flying.lars@gmail.com>
2018-04-15 00:35:48 +0200

    add app post handling

--- src/index.js
+++ src/index.js
@@ -3,8 +3,12 @@ import path from 'path';
 
 const app = express();
 
+app.post('/api/auth', (req, res) => {
+  res.status(400).json({errors: {global: "Invalid credentials" }})
+});
+
 app.get('/*', (req, res) => {
   res.sendFile(path.join(__dirname, 'index.html'));
-})
+});
 
 app.listen(8080, () => console.log('running 8080'));
