6d3a0f4d297d1520293ac014c12001d6f717589d
Lars Wenk <flying.lars@gmail.com>
2018-04-17 10:35:26 +0200

    add confirmation handling

--- src/index.js
+++ src/index.js
@@ -2,17 +2,21 @@ import express from 'express';
 import path from 'path';
 import mongoose from 'mongoose';
 import bodyParser from 'body-parser';
-import auth from './routes/auth';
 import dotenv from 'dotenv';
 import Promise from 'bluebird';
+
+import auth from './routes/auth';
+import users from "./routes/users";
 
 dotenv.config();
 const app = express();
 app.use(bodyParser.json());
 mongoose.Promise = Promise;
-mongoose.connect(process.env.MONGODB_URL, { useMongoClient: true });
+mongoose.connect(process.env.MONGODB_URL);
 
+/* mounting */
 app.use('/api/auth', auth);
+app.use("/api/users", users);
 
 app.get('/*', (req, res) => {
   res.sendFile(path.join(__dirname, 'index.html'));
--- src/models/User.js
+++ src/models/User.js
@@ -1,31 +1,59 @@
-import mongoose from 'mongoose';
-import bcrypt from 'bcrypt';
-import jsonwebtoken from 'jsonwebtoken';
+import mongoose from "mongoose";
+import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
+import uniqueValidator from "mongoose-unique-validator";
 
-const schema = new mongoose.Schema({
-    email: { type: String, required: true, lowercase: true, index: true },
-    passwordHash: { type: String, required: true }
+const schema = new mongoose.Schema(
+  {
+    email: {
+      type: String,
+      required: true,
+      lowercase: true,
+      index: true,
+      unique: true
+    },
+    passwordHash: { type: String, required: true },
+    confirmed: { type: Boolean, default: false },
+    confirmationToken: { type: String, default: "" }
   },
   { timestamps: true }
 );
 
 schema.methods.isValidPassword = function isValidPassword(password) {
   return bcrypt.compareSync(password, this.passwordHash);
-}
+};
+
+schema.methods.setPassword = function setPassword(password) {
+  this.passwordHash = bcrypt.hashSync(password, 10);
+};
+
+schema.methods.setConfirmationToken = function setConfirmationToken() {
+  this.confirmationToken = this.generateJWT();
+};
+
+schema.methods.generateConfirmationUrl = function generateConfirmationUrl() {
+  return `${process.env.HOST}/confirmation/${this.confirmationToken}`;
+};
 
 schema.methods.generateJWT = function generateJWT() {
-  return jsonwebtoken.sign({
-    email: this.email
-  },
-  process.env.JWT_SECRET
-);
-}
+  return jwt.sign(
+    {
+      email: this.email,
+      confirmed: this.confirmed
+    },
+    process.env.JWT_SECRET
+  );
+};
+
 
 schema.methods.toAuthJSON = function toAuthJSON() {
   return {
     email: this.email,
+    confirmed: this.confirmed,
     token: this.generateJWT()
-  }
+  };
 };
 
-export default mongoose.model('User', schema);
+schema.plugin(uniqueValidator, { message: "This email is already taken" });
+
+export default mongoose.model("User", schema);
