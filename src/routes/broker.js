567a1b577256d35dad6f25be3c478ef0f5ead508
Lars Wenk <flying.lars@gmail.com>
2018-04-23 22:48:35 +0200

    wording adaption

--- src/routes/broker.js
+++ src/routes/broker.js
@@ -1,6 +1,5 @@
 import express from "express";
 import request from "request-promise";
-import { parseString } from "xml2js";
 import authenticate from "../middlewares/authenticate";
 import Book from "../models/Broker";
 import parseErrors from "../utils/parseErrors";
@@ -9,12 +8,12 @@ const router = express.Router();
 router.use(authenticate);
 
 router.get("/", (req, res) => {
-  Book.find({ userId: req.currentUser._id }).then(broker => res.json({ books }));
+  Broker.find({ userId: req.currentUser._id }).then(broker => res.json({ books }));
 });
 
 router.post("/", (req, res) => {
-  Book.create({ ...req.body.book, userId: req.currentUser._id })
-    .then(book => res.json({ book }))
+  Book.create({ ...req.body.broker, userId: req.currentUser._id })
+    .then(book => res.json({ broker }))
     .catch(err => res.status(400).json({ errors: parseErrors(err.errors) }));
 });
 
